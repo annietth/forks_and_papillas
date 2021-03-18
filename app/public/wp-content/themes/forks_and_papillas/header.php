af78aef07308cb67f238f6544beab847de06b958
anthony-roux <roux.anthony@hetic.net>
2021-03-18 22:47:10 +0100

    create header for test

--- app/public/wp-content/themes/forks_and_papillas/header.php
+++ app/public/wp-content/themes/forks_and_papillas/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="<?php echo LANG; ?>">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
@@ -10,3 +10,16 @@
 <body <?php body_class(); ?>>
     
     <?php wp_body_open(); ?>
+
+    <header id="header" class="header">
+        <div class="header__container">
+            <div class="header__logo">
+                <div class="logo"></div>
+            </div>
+            <ul class="header__items">
+                <li class="header__item">Accueil</li>
+                <li class="header__item">Qui sommes nous ?</li>
+                <li class="header__item">Contact</li>
+            </ul>
+        </div>
+    </header>
