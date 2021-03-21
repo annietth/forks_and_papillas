e09c9c2b14d7749c4e5c472f6a33c80a4a378da7
Annie Tran <annie.tran@hetic.net>
2021-03-21 21:21:13 +0100

    Add Hero for home page

---
+++ app/public/wp-content/themes/forks_and_papillas/template-parts/hero.php
@@ -0,0 +1,25 @@
+
+<div class="hero">
+    <div class="hero__image">
+        <img src= "<?php echo get_template_directory_uri(); ?>/img/hero_mbl.png" alt="image_of_hero">
+         <!-- <img class="image__desktop"src="img/hero_dsktp.png" alt="image_of_desktop"> -->
+    </div>
+    <div class="hero__texts">
+        <h1>Forks And Papillas</h1>
+        <ul class="hero__texts__anime">
+            <li>Cuisine Oriental</li>
+            <li>Cuisine Vietnamienne</li>
+            <li>Cuisine Française</li>
+            <li>Cuisine Créole</li>
+            <li>Cuisine Italienne</li>
+            <li>Cuisine Indonésienne</li>
+        </ul>
+        <h2 class="hero__text">
+            Choisissez <span>le meilleur</span>. <br>
+            Plus de <span>100 milles </span>restaurants!
+        </h2>
+        <p>À la recherche du bon restaurent, en cette pandémie aider vos restaurateurs en les soutenants au mieux ! Nous sommes en collaborations avec plusieurs chefs et proposons le meilleurs pour vous.</p>
+        <p class="hero__text">Suivez notre Newsletter pour ne rater aucune nouvelles</p>
+    </div>
+</div>
+
