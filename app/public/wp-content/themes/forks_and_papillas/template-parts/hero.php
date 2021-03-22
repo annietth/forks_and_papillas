c8f0a91af38c5da41a8d49819bf7beb0bf32488a
Annie Tran <annie.tran@hetic.net>
2021-03-22 01:31:07 +0100

    Finish hero mobile and stard sub hero

--- app/public/wp-content/themes/forks_and_papillas/template-parts/hero.php
+++ app/public/wp-content/themes/forks_and_papillas/template-parts/hero.php
@@ -1,20 +1,35 @@
+<marquee class="hero__anime" scrollamount="5" width="500">
+    Cuisine Oriental - Cuisine Vietnamienne - Cuisine Française - Cuisine Créole -  Cuisine Italienne - Cuisine Indonésienne
+</marquee> 
+<br>
+<br>
+<div class="hero"> 
 
-<div class="hero">
-    
-    <img style="width:100%"src= "<?php echo get_template_directory_uri(); ?>/img/hero_mbl.png" alt="image_of_hero">
-    <img class="hero__desktop"src="img/hero_dsktp.png" alt="image_of_desktop">
-    
-    <div class="hero__texts">
-        <h1>Forks And Papillas</h1>
-        <marquee class="hero__texts__anime" scrollamount="5" width="400">
-            Cuisine Oriental - Cuisine Vietnamienne - Cuisine Française - Cuisine Créole -  Cuisine Italienne - Cuisine Indonésienne
-        </marquee>
+     <div class="hero__texts">
+
         <h2 class="hero__text">
             Choisissez <span>le meilleur</span>. <br>
             Plus de <span>100 milles </span>restaurants!
         </h2>
-        <p>À la recherche du bon restaurent, en cette pandémie aider vos restaurateurs en les soutenants au mieux ! Nous sommes en collaborations avec plusieurs chefs et proposons le meilleurs pour vous.</p>
-        <p class="hero__text">Suivez notre Newsletter pour ne rater aucune nouvelles</p>
+
+        <button>Recherche</button>
+        
     </div>
+    <div class="hero__image">
+        <img style="width:100%"src= "<?php echo get_template_directory_uri(); ?>/img/hero_mbl.png" alt="image_of_hero">
+        <!-- <img class="hero__desktop" style="width:100%"src= "<?php echo get_template_directory_uri(); ?>/img/hero_dsktp.png" alt="image_of_desktop"> -->
+    </div>
+</div>
+
+<div class="subhero">
+    <p>
+        À la recherche du bon restaurent, en cette pandémie aider vos restaurateurs en les soutenants au mieux ! Nous sommes en collaborations avec plusieurs chefs et proposons le meilleurs pour vous.
+
+    </p>
+    <br>
+    <br>
+    <p>
+        Suivez notre Newsletter pour ne rater aucune nouvelles
+    </p>
 </div>
 
