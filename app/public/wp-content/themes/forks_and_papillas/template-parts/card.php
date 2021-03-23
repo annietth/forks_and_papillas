d1a6adea6ab7864efd53e2f86b75e39d06f7e4e5
anthony-roux <roux.anthony@hetic.net>
2021-03-23 23:28:28 +0100

    MAJ card

--- app/public/wp-content/themes/forks_and_papillas/template-parts/card.php
+++ app/public/wp-content/themes/forks_and_papillas/template-parts/card.php
@@ -1,80 +1,36 @@
-<div class="container">
-  <div class="box">
-      <div class="card__header">
-      <img class=""src="<?php echo get_template_directory_uri(); ?>img/Vector.png" alt="label">
-      </div>
-      <div class="card__body">
-        <p class="cardcategory__name">Name of category</p>
-        <div class="line"></div>
-        <p class="cardrestaurant__name">Restaurant name</p>
-        <div class="stars">
-        <img class=""src="<?php echo get_template_directory_uri(); ?> img/Stars_average.png" alt="stars">
+<?php 
+$taxonomies = get_the_terms(get_the_ID(), 'type-cuisine'); 
+
+if($taxonomies) {
+  $taxonomy = $taxonomies[0]->name;
+  } else {
+  $taxonomy = "";
+}
+
+?>
+
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <a href="<?php esc_url( the_permalink() ) ?>" class="box">
+        <div class="card__header">
+          <?php if ( has_post_thumbnail() ) {
+            the_post_thumbnail('listing-card');
+          } else { ?>
+            <!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/img/pizza.png" alt="<?php the_title(); ?>"> -->
+          <?php } ?>
+          <div class="card__body">
+            <p class="cardcategory__name">
+            <?php echo $taxonomy;?>
+            </p>
+            <div class="line"></div>
+            <p class="cardrestaurant__name">				
+              <?php the_title(); ?>
+            </p>
+            <div class="stars">
+            <img class=""src="<?php echo get_template_directory_uri(); ?>/img/Stars_average.png" alt="stars">
+            </div>
+          </div>
         </div>
-      </div>
-  </div>
-  <div class="box">
-      <div class="card__header">
-      <img class=""src="<?php echo get_template_directory_uri(); ?>img/Vector.png" alt="label">
-      </div>
-      <div class="card__body">
-        <p class="cardcategory__name">Name of category</p>
-        <div class="line"></div>
-        <p class="cardrestaurant__name">Restaurant name</p>
-        <div class="stars">
-        <img class=""src="img/Stars_average.png" alt="stars">
-        </div>
-      </div>
-  </div>
-  <div class="box">
-      <div class="card__header">
-      <img class=""src="img/Vector.png" alt="label">
-      </div>
-      <div class="card__body">
-        <p class="cardcategory__name">Name of category</p>
-        <div class="line"></div>
-        <p class="cardrestaurant__name">Restaurant name</p>
-        <div class="stars">
-        <img class=""src="img/Stars_average.png" alt="stars">
-        </div>
-      </div>
-  </div>
-  <div class="box">
-      <div class="card__header">
-      <img class=""src="img/Vector.png" alt="label">
-      </div>
-      <div class="card__body">
-        <p class="cardcategory__name">Name of category</p>
-        <div class="line"></div>
-        <p class="cardrestaurant__name">Restaurant name</p>
-        <div class="stars">
-        <img class=""src="img/Stars_average.png" alt="stars">
-        </div>
-      </div>
-  </div>
-  <div class="box">
-      <div class="card__header">
-      <img class=""src="img/Vector.png" alt="label">
-      </div>
-      <div class="card__body">
-        <p class="cardcategory__name">Name of category</p>
-        <div class="line"></div>
-        <p class="cardrestaurant__name">Restaurant name</p>
-        <div class="stars">
-        <img class=""src="img/Stars_average.png" alt="stars">
-        </div>
-      </div>
-  </div>
-  <div class="box">
-      <div class="card__header">
-      <img class=""src="img/Vector.png" alt="label">
-      </div>
-      <div class="card__body">
-        <p class="cardcategory__name">Name of category</p>
-        <div class="line"></div>
-        <p class="cardrestaurant__name">Restaurant name</p>
-        <div class="stars">
-        <img class=""src="img/Stars_average.png" alt="stars">
-        </div>
-      </div>
-  </div>
-</div
+    </a>
+</article>
