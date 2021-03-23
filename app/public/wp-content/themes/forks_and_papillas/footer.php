62c17275dfc90f0c0f0bf2254629bd510fdfb836
Annie Tran <annie.tran@hetic.net>
2021-03-23 20:59:40 +0100

    begining footer

--- app/public/wp-content/themes/forks_and_papillas/footer.php
+++ app/public/wp-content/themes/forks_and_papillas/footer.php
@@ -1,3 +1,14 @@
 <?php wp_footer(); ?>
+
+<div id="footer" class="footer">
+    <div class="footer__container">
+        <div class="footer__logo">
+            <a href="<?= get_home_url() ?>">
+                    <img class="logo" src="<?= get_template_directory_uri(); ?>/img/logo_fp.svg"></img>
+            </a>
+        </div>
+        
+    </div>
+</div>
 </body>
 </html>
