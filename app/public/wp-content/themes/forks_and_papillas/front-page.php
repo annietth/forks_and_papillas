ece3e9cbf8ac5a026a2a9e04bc278fd803cd8fe6
dorian-vidal <dorian.vidal@hetic.net>
2021-03-23 19:55:45 +0100

    add card_groups

--- app/public/wp-content/themes/forks_and_papillas/front-page.php
+++ app/public/wp-content/themes/forks_and_papillas/front-page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 	 
 	<?php get_template_part('template-parts/hero','none') ?>
+	<?php get_template_part('template-parts/card','none') ?>
 
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
     
