0fd9e5d0e3e7615a50b5d420ad940a56bdac70d6
anthony-roux <roux.anthony@hetic.net>
2021-03-25 15:33:13 +0100

    finish card

--- app/public/wp-content/themes/forks_and_papillas/template-parts/card.php
+++ app/public/wp-content/themes/forks_and_papillas/template-parts/card.php
@@ -1,5 +1,5 @@
 <?php 
-$taxonomies = get_the_terms(get_the_ID(), 'type-cuisine'); 
+$taxonomies = get_the_terms(get_the_ID(), 'type_cuisine'); 
 
 if($taxonomies) {
   $taxonomy = $taxonomies[0]->name;
@@ -7,58 +7,55 @@ if($taxonomies) {
   $taxonomy = "";
 }
 
+$top_restaurant = get_field('top_restaurant');
+$grade = get_field('grade');
+
+
 ?>
 
 
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a href="<?php esc_url( the_permalink() ) ?>" class="box">
-    <div class="card__header">
+ 
 
-      <?php if ( has_post_thumbnail() ) {
-        the_post_thumbnail('listing-card');
-      } else { ?>
-        <!-- <img class="" src="<?php echo get_template_directory_uri(); ?>/img/pizza.png" alt="<?php the_title(); ?>"> -->
-      <?php } ?>
+<?php 
+if ( has_post_thumbnail() ) { ?>
+<article  class="card" style="background-image: url('<?= the_post_thumbnail_url('listing-card'); ?>');"> 
+<?php } else { ?>
+  <article  class="card" style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/pizza.jpg');"> 
+<?php } ?>
 
-      <div class="card__group">  
+  <a  
+    href="<?php esc_url( the_permalink() ) ?>" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-        <div class="card__body">
-          <div class="card__image">
-            <img src= "<?php echo get_template_directory_uri(); ?>/img/pizza.png" alt="food">
-            <!-- <div class="mark">
-              <img class=""src="<?php echo get_template_directory_uri(); ?>/img/vector.png" alt="bookmark_favorite">
-          </div> -->
-          </div>
-          
+    <?php if(get_field('top_restaurant') == 1): ?>
+      <img class="topicon" src="<?php echo get_template_directory_uri(); ?>/img/topicon.svg" alt="Top restaurant">
+    <?php endif; ?> 
+  
+  
+    <div class="card__band">
+      <p class="card__category"><?php echo $taxonomy;?></p>
+      <div class="card__line"></div>
+      <h3 class="card__restaurant"><?php the_title(); ?></h3>
 
-          <div class="cardname">
+      <?php if(get_field('grade') == '1star') { ?>
+      <img src="<?php echo get_template_directory_uri(); ?>/img/1star.svg" alt="1 etoile">
 
-            <p class="cardcategory__name">
-              <?php echo $taxonomy;?>
-            </p>
+      <?php } else if(get_field('grade') == '2stars') { ?>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/2stars.svg" alt="2 etoiles">
 
-            <div class="line"></div>
+      <?php } else if(get_field('grade') == '3stars') { ?>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/3stars.svg" alt="3 etoiles">
 
-            <p class="cardrestaurant__name">	
+      <?php } else if(get_field('grade') == '4stars') { ?>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/4stars.svg" alt="4 etoiles">
 
-             <?php the_title(); ?>
-            </p>
+      <?php } else if(get_field('grade') == '5stars') { ?>
 
-            <div class="line"></div>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/5stars.svg" alt="5 etoiles">
 
-            <br>
-
-            <div class="stars">
-              <img class=""src="<?php echo get_template_directory_uri(); ?>/img/Stars_average.png" alt="stars">
-            </div>
-
-          </div>
-
-        </div>
-      </div>
+      <?php }; ?> 
     </div>
-
   </a>
+  </article>
 
-</article>
+
