893ddb2bb38aaa6200d827b267f635d10e05d2b5
anthony-roux <roux.anthony@hetic.net>
2021-03-27 23:13:46 +0100

    refont HERO html and CSS

--- app/public/wp-content/themes/forks_and_papillas/template-parts/hero.php
+++ app/public/wp-content/themes/forks_and_papillas/template-parts/hero.php
@@ -18,30 +18,29 @@
             <?= get_field('first_sentences', 'options'); ?>
         </h1>
 
-        <div class="hero__image">
-            <img class="hero__image__mobile" style="width:100%"src= "<?php echo get_template_directory_uri(); ?>/img/hero_mbl.png" alt="image_of_hero">
-            <img class="hero__image__desktop" style="width:100%"src= "<?php echo get_template_directory_uri(); ?>/img/hero_dktp.png" alt="image_of_desktop">
+        <button class="hero__ctamobile">Recherche</button>
+        <div class="hero__subhero">
+            <p>
+                <?= get_field('description', 'options'); ?>
+            </p>
+
+            <p class="hero__newsletter">
+                <?= get_field('follow_newsletter', 'options'); ?>
+            </p>
+            <form class="subhero__input">
+                <label for="mail"></label>
+                <input type="email" id="mail" name="user_email" placeholder="exemple@gmail.com">
+                <button class="subhero__input__button"><img src= "<?php echo get_template_directory_uri(); ?>/img/enter_arrow.svg" alt="enter"></button>
+            </form>
         </div>
-        
-        
-        
-    </div>    
-    <button class="hero__mobile">Recherche</button>
-    <div class="hero__subhero">
-        <p>
-            <?= get_field('description', 'options'); ?>
-        </p>
-        <br>
-        <br>
-        <p>
-            <?= get_field('follow_newsletter', 'options'); ?>
-        </p>
-        <form class="subhero__input">
-            <label for="mail"></label>
-            <input type="email" id="mail" name="user_email" placeholder="exemple@gmail.com">
-            <button class="subhero__input__button"><img src= "<?php echo get_template_directory_uri(); ?>/img/enter_arrow.svg" alt="enter"></button>
-        </form>
+    </div>  
+
+    <div class="hero__image">
+        <img class="hero__image__desktop" style="width:100%"src= "<?php echo get_template_directory_uri(); ?>/img/hero_dktp.png" alt="image_of_desktop">
     </div>
+
+
+      
 
 
 
